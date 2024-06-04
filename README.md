6f46bddd6c30997b69d3fff19067c3601fc42384
KraXen72 <dpelech1@gmail.com>
2024-06-04 18:36:06 +0200

    readme & test

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # inlang-t-function-jsx-hybrid-matcher
 
-this pluing is a modificaiton of the official `t-function matcher` plugin, with the following changes:
+this plugin is a modificaiton of the official `t-function matcher` plugin, with the following changes:
 - support several custom translate function names (instead of just `t`)
 - TODO: support custom JSX attributes on react components (later)
--- src/settings.ts
+++ src/settings.ts
@@ -1,19 +1,22 @@
 import { Type, type Static } from '@sinclair/typebox'
 
 
-export const PluginSettings = Type.Object({
-	preferredTfuncName: Type.Optional(Type.String({
-		title: "Preferred t-function name",
-		description: 'The t("key") function name to use when extracting a new string.',
-		default: "t",
-		examples: ["t", "translate", "i18n.t"]
-	})),
-	recognizedTfuncNames: Type.Optional(Type.Array(Type.String(), {
-		title: "Recognized t-function names",
-		description: 'Array of recognized t("key") function names (to show inline previews of messages)',
-		default: ["t"],
-		examples: ["t", "translate", "i18n.t"]
-	}))
-})
+// export const PluginSettings = Type.Object({
+// 	preferredTfuncName: Type.Optional(Type.String({
+// 		title: "Preferred t-function name",
+// 		description: 'The t("key") function name to use when extracting a new string.',
+// 		default: "t",
+// 		examples: ["t", "translate", "i18n.t"]
+// 	})),
+// 	recognizedTfuncNames: Type.Optional(Type.Array(Type.String(), {
+// 		title: "Recognized t-function names",
+// 		description: 'Array of recognized t("key") function names (to show inline previews of messages)',
+// 		default: ["t"],
+// 		examples: ["t", "translate", "i18n.t"]
+// 	}))
+// })
+// export type PluginSettings = Static<typeof PluginSettings>
 
-export type PluginSettings = Static<typeof PluginSettings>
+export const PluginSettings = {}
+export type PluginSettings = {}
+
