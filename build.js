51ce54b0f7452f2e2fe38709b4b3867e093b2c32
KraXen72 <dpelech1@gmail.com>
2025-07-14 19:48:44 +0200

    gsync 2025_07_14 19_26_46 (from alias)

--- build.js
+++ build.js
@@ -1,52 +1,52 @@
 import { context } from "esbuild";
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-// import { readFileSync } from 'node:fs';
-// import { extname, dirname as _dirname } from "node:path";
+import { readFileSync } from 'node:fs';
+import { extname, dirname as _dirname } from "node:path";
 
 
-// const nodeModules = new RegExp(
-//   /^(?:.*[\\/])?node_modules(?:\/(?!postgres-migrations).*)?$/
-// );
+const nodeModules = new RegExp(
+  /^(?:.*[\\/])?node_modules(?:\/(?!postgres-migrations).*)?$/
+);
 
-// const dirnamePlugin = {
-//   name: 'dirname',
-//   setup(build) {
-//     build.onLoad({ filter: /.*/ }, ({ path: filePath }) => {
-//       if (!filePath.match(nodeModules)) {
-//         let contents = readFileSync(filePath, 'utf8');
-//         const loader = extname(filePath).substring(1);
-//         const dirname = _dirname(filePath);
-//         contents = contents
-//           .replace('__dirname', `"${dirname}"`)
-//           .replace('__filename', `"${filePath}"`);
-//         return {
-//           contents,
-//           loader,
-//         };
-//       }
-//     });
-//   },
-// };
+const dirnamePlugin = {
+  name: 'dirname',
+  setup(build) {
+    build.onLoad({ filter: /.*/ }, ({ path: filePath }) => {
+      if (!filePath.match(nodeModules)) {
+        let contents = readFileSync(filePath, 'utf8');
+        const loader = extname(filePath).substring(1);
+        const dirname = _dirname(filePath);
+        contents = contents
+          .replace('__dirname', `"${dirname}"`)
+          .replace('__filename', `"${filePath}"`);
+        return {
+          contents,
+          loader,
+        };
+      }
+    });
+  },
+};
 
 // eslint-disable-next-line no-undef
 const isProduction = process.env.NODE_ENV === "production";
 
-const banner = `
-	import { createRequire as shim_createRequire } from 'module';
-	import { fileURLToPath as shim_fileURLToPath  } from 'url';
-	const require = shim_createRequire(import.meta.url);
-	const __filename = shim_fileURLToPath(import.meta.url);
-	const __dirname = import.meta.dirname;`
-	.split("\n")
-	.filter(Boolean)
-	.map(l => l.trim().endsWith(";") ? l.trim() : l.trim() + ";")
-	.join(isProduction ? " " : "\n");
+// const banner = `
+// 	import { createRequire as shim_createRequire } from 'module';
+// 	import { fileURLToPath as shim_fileURLToPath  } from 'url';
+// 	const require = shim_createRequire(import.meta.url);
+// 	const __filename = shim_fileURLToPath(import.meta.url);
+// 	const __dirname = import.meta.dirname;`
+// 	.split("\n")
+// 	.filter(Boolean)
+// 	.map(l => l.trim().endsWith(";") ? l.trim() : l.trim() + ";")
+// 	.join(isProduction ? " " : "\n");
 
 const ctx = await context({
 	entryPoints: ["./src/index.ts"],
-	banner: {
-		js: banner
-	},
+	// banner: {
+	// 	js: banner
+	// },
 	outdir: "./dist",
 	minify: isProduction,
 	target: "es2022",
@@ -68,7 +68,8 @@ const ctx = await context({
 					path: new URL('./mocks/tty.js', import.meta.url).pathname
 				}));
 			},
-		}
+		},
+		dirnamePlugin
 	],
 });
 
