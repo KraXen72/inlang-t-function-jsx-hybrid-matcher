8dfc9341c263b2c32fbf51235180e1b557ff778d
KraXen72 <dpelech1@gmail.com>
2025-07-14 19:47:37 +0200

    me when i forget to push the build

--- build.js
+++ build.js
@@ -1,25 +1,65 @@
 import { context } from "esbuild";
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+// import { readFileSync } from 'node:fs';
+// import { extname, dirname as _dirname } from "node:path";
+
+
+// const nodeModules = new RegExp(
+//   /^(?:.*[\\/])?node_modules(?:\/(?!postgres-migrations).*)?$/
+// );
+
+// const dirnamePlugin = {
+//   name: 'dirname',
+//   setup(build) {
+//     build.onLoad({ filter: /.*/ }, ({ path: filePath }) => {
+//       if (!filePath.match(nodeModules)) {
+//         let contents = readFileSync(filePath, 'utf8');
+//         const loader = extname(filePath).substring(1);
+//         const dirname = _dirname(filePath);
+//         contents = contents
+//           .replace('__dirname', `"${dirname}"`)
+//           .replace('__filename', `"${filePath}"`);
+//         return {
+//           contents,
+//           loader,
+//         };
+//       }
+//     });
+//   },
+// };
 
 // eslint-disable-next-line no-undef
 const isProduction = process.env.NODE_ENV === "production";
 
+const banner = `
+	import { createRequire as shim_createRequire } from 'module';
+	import { fileURLToPath as shim_fileURLToPath  } from 'url';
+	const require = shim_createRequire(import.meta.url);
+	const __filename = shim_fileURLToPath(import.meta.url);
+	const __dirname = import.meta.dirname;`
+	.split("\n")
+	.filter(Boolean)
+	.map(l => l.trim().endsWith(";") ? l.trim() : l.trim() + ";")
+	.join(isProduction ? " " : "\n");
+
 const ctx = await context({
 	entryPoints: ["./src/index.ts"],
+	banner: {
+		js: banner
+	},
 	outdir: "./dist",
 	minify: isProduction,
-	// ----------------------------------
-	// allow top level await
-	// https://caniuse.com/mdn-javascript_operators_await_top_level
 	target: "es2022",
-	// inlang does not support import maps
 	bundle: true,
-	// esm to work in the browser
 	format: "esm",
-	platform: isProduction ? "browser" : "node",
-	// sourcemaps are unused at the moment
+	//! extremly important to be platform neutral
+	//! to ensure that modules run in browser
+	//! and server contexts.
+	platform: "neutral",
+	mainFields: ["module", "main"],
+	external: ["typescript", "debug", "fast-glob", "inspector", "globby"],
 	sourcemap: false,
-	plugins: isProduction ? [
+	plugins: [
 		NodeModulesPolyfillPlugin(),
 		{
 			name: 'tty-mock',
@@ -29,7 +69,7 @@ const ctx = await context({
 				}));
 			},
 		}
-	] : [],
+	],
 });
 
 if (isProduction === false) {
